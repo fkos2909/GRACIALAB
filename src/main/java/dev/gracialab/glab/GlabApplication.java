8180041d3f40256017ce535b3f74121fc267061d
Fatima <fatima.ospina@gmail.com>
2023-07-17 20:23:43 -0400

    first commit

---
+++ src/main/java/dev/gracialab/glab/GlabApplication.java
@@ -0,0 +1,13 @@
+package dev.gracialab.glab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlabApplication.class, args);
+	}
+
+}
