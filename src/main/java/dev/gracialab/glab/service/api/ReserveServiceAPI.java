66894b81b312290e933b9a3eacbeb8150c1282e3
Fatima <fatima.ospina@gmail.com>
2023-07-18 13:37:34 -0400

    Logica de Reserve (only reserve)

---
+++ src/main/java/dev/gracialab/glab/service/api/ReserveServiceAPI.java
@@ -0,0 +1,8 @@
+package dev.gracialab.glab.service.api;
+
+import dev.gracialab.glab.commons.GenericServiceAPI;
+import dev.gracialab.glab.model.Reserve;
+
+public interface ReserveServiceAPI extends GenericServiceAPI<Reserve, Long>{
+    
+}
---
+++ src/main/java/dev/gracialab/glab/service/impl/ReserveServiceImpl.java
@@ -0,0 +1,21 @@
+package dev.gracialab.glab.service.impl;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import dev.gracialab.glab.commons.GenericServiceImpl;
+import dev.gracialab.glab.dao.api.ReserveDaoAPI;
+import dev.gracialab.glab.model.Reserve;
+import dev.gracialab.glab.service.api.ReserveServiceAPI;
+
+@Service
+public class ReserveServiceImpl extends GenericServiceImpl<Reserve, Long> implements ReserveServiceAPI{
+
+    private ReserveDaoAPI reserveDaoAPI;
+
+    @Override
+    public CrudRepository<Reserve, Long> getDao() {
+        return reserveDaoAPI;
+    }
+    
+}
