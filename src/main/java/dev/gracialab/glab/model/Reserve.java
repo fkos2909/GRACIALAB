26f4ff917f9c9b08aef1d6c665a5f2538008d0dc
Fatima <fatima.ospina@gmail.com>
2023-07-18 10:46:38 -0400

    Creando models

---
+++ src/main/java/dev/gracialab/glab/model/Reserve.java
@@ -0,0 +1,92 @@
+package dev.gracialab.glab.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Reserve {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private java.sql.Date date;
+
+    @Column
+    private java.sql.Time hour;
+
+    @Column
+    private String state;
+
+    @Column
+    private String reserve_type;
+
+    @Column
+    private Integer number_of_people;
+
+    @Column
+    private String note;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public java.sql.Date getDate() {
+        return date;
+    }
+
+    public void setDate(java.sql.Date date) {
+        this.date = date;
+    }
+
+    public java.sql.Time getHour() {
+        return hour;
+    }
+
+    public void setHour(java.sql.Time hour) {
+        this.hour = hour;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getReserve_type() {
+        return reserve_type;
+    }
+
+    public void setReserve_type(String reserve_type) {
+        this.reserve_type = reserve_type;
+    }
+
+    public Integer getNumber_of_people() {
+        return number_of_people;
+    }
+
+    public void setNumber_of_people(Integer number_of_people) {
+        this.number_of_people = number_of_people;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    
+
+}
---
+++ src/main/java/dev/gracialab/glab/model/User.java
@@ -0,0 +1,103 @@
+package dev.gracialab.glab.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String last_name;
+
+    @Column
+    private String document_type;
+
+    @Column
+    private String document;
+
+    @Column
+    private String mail;
+
+    @Column
+    private String password;
+
+    @Column
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getDocument_type() {
+        return document_type;
+    }
+
+    public void setDocument_type(String document_type) {
+        this.document_type = document_type;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+
+}
